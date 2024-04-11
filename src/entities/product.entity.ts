d19b16b8e7d880de4747c93bb9cec6edca58e1e1
jeanmbiz <jeanmbiz@hotmail.com>
2024-04-11 09:09:19 -0300

    feat: add class validator on entities

--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -6,6 +6,17 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
+import {
+  IsDate,
+  IsDecimal,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Length,
+} from "class-validator";
 
 @Entity("products")
 class Product {
@@ -13,39 +24,64 @@ class Product {
   id: string;
 
   @Column({ length: 100 })
+  @IsString({ message: "name deve ser uma string" })
+  @IsNotEmpty()
+  @Length(1, 100, { message: "o nome deve ter entre 1 e 100 caracteres" })
   name: string;
 
   @Column({ length: 100 })
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 100)
   brand: string;
 
   @Column({ length: 100 })
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 100)
   store: string;
 
   @Column({ type: "int" })
+  @IsPositive()
+  @IsInt()
   quantity: number;
 
   @Column({ type: "decimal", precision: 10, scale: 2 })
+  @IsPositive()
+  @IsDecimal()
   price_un: number;
 
   @Column({ type: "decimal", precision: 10, scale: 2 })
+  @IsNumber()
   freight: number;
 
   @Column({ type: "int", nullable: true })
+  @IsNumber()
   durability_days: number;
 
   @Column({ type: "decimal", precision: 10, scale: 2 })
+  @IsNumber()
+  @IsPositive()
   purchase_value: number;
 
   @Column({ type: "date" })
+  @IsDate()
   purchase_date: Date;
 
   @Column({ type: "date", nullable: true })
+  @IsOptional()
+  @IsDate()
   initial_date: Date;
 
   @Column({ type: "date", nullable: true })
+  @IsOptional()
+  @IsDate()
   final_date: Date;
 
   @Column({ length: 100, nullable: true })
+  @IsString()
+  @IsOptional()
+  @Length(0, 100)
   observation: string;
 
   @CreateDateColumn()
